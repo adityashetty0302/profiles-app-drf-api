9656cbd2f3fcc5377a8e1efd333ef08dfaa49c06
adityashetty0302 <adityashetty0302@gmail.com>
2019-08-05 23:35:46 +0530

    added permissions & authentications

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from . import serializers, models
+from . import serializers, models, permissions
 from rest_framework import status, viewsets
 
 
@@ -106,3 +106,4 @@ class UserProfileViewset(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    permission_classes = (permissions.IsOwnerOrReadOnly,)
